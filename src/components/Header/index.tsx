402c069cc6a1fbc2c1c332c24c87c5afa895e9af
joao.lacerda <joao.lacerda@fitbank.com.br>
2022-01-26 09:55:58 -0300

    Feature: iniciando estrutura do Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
+import Modal from  'react-modal'
 import { Container, Content } from './styles'
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+    
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
+
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova transação
                 </button>
+
+                <Modal 
+                    isOpen={isNewTransactionModalOpen}
+                    onRequestClose={handleCloseNewTransactionModal}    
+                >
+                    <h2>Cadastrar transação</h2>
+                </Modal>
             </Content>
         </Container>
     )
